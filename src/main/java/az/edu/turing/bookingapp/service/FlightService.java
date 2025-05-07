e0f6aa03f0a937a89d0a22e1118f6cb731756b24
farajzada <mfarajzadaa@gmail.com>
2025-05-07 18:10:27 +0400

    initial commit

--- src/main/java/az/edu/turing/bookingapp/service/FlightService.java
+++ src/main/java/az/edu/turing/bookingapp/service/FlightService.java
@@ -1,4 +1,13 @@
 package az.edu.turing.bookingapp.service;
 
+import az.edu.turing.bookingapp.domain.entity.FlightEntity;
+
+import java.util.List;
+
 public interface FlightService {
+    List<FlightEntity> getAllFlights();
+    FlightEntity getFlightById(Long id);
+    FlightEntity createFlight(FlightEntity flight);
+    FlightEntity updateFlight(Long id, FlightEntity updatedFlight);
+    void deleteFlight(Long id);
 }
--- src/main/java/az/edu/turing/bookingapp/service/impl/FlightServiceImpl.java
+++ src/main/java/az/edu/turing/bookingapp/service/impl/FlightServiceImpl.java
@@ -1,4 +1,56 @@
 package az.edu.turing.bookingapp.service.impl;
 
-public class FlightServiceImpl {
+import az.edu.turing.bookingapp.domain.entity.FlightEntity;
+import az.edu.turing.bookingapp.domain.repository.FlightDao;
+import az.edu.turing.bookingapp.service.FlightService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightServiceImpl implements FlightService {
+    private final FlightDao flightDao;
+
+    public FlightServiceImpl(FlightDao flightDao) {
+        this.flightDao = flightDao;
+    }
+
+    @Override
+    public List<FlightEntity> getAllFlights() {
+        return flightDao.findAll();
+    }
+
+    @Override
+    public FlightEntity getFlightById(Long id) {
+        return flightDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Flight not found with id: " + id));
+
+    }
+
+    @Override
+    public FlightEntity createFlight(FlightEntity flight) {
+        return flightDao.save(flight);
+    }
+
+
+    @Override
+    public FlightEntity updateFlight(Long id, FlightEntity updatedFlight) {
+        FlightEntity flight = flightDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Flight not found with id: " + id));
+
+        flight.setOrigin(updatedFlight.getOrigin());
+        flight.setDestination(updatedFlight.getDestination());
+        flight.setAvailableSeats(updatedFlight.getAvailableSeats());
+        flight.setTimestamp(updatedFlight.getTimestamp());
+
+        return flightDao.save(flight);
+    }
+
+    @Override
+    public void deleteFlight(Long id) {
+        if (!flightDao.existsById(id)) {
+            throw new RuntimeException("Flight not found with id: " + id);
+        }
+        flightDao.deleteById(id);
+    }
 }

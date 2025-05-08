d580271c2f751a34ca1d210d30232f68bed12a12
farajzada <mfarajzadaa@gmail.com>
2025-05-08 17:39:46 +0400

    initial commit

--- src/main/java/az/edu/turing/bookingapp/controller/FlightController.java
+++ src/main/java/az/edu/turing/bookingapp/controller/FlightController.java
@@ -1,11 +1,15 @@
 package az.edu.turing.bookingapp.controller;
 
 import az.edu.turing.bookingapp.domain.entity.FlightEntity;
+import az.edu.turing.bookingapp.model.request.FlightRequest;
+import az.edu.turing.bookingapp.model.response.FlightResponse;
 import az.edu.turing.bookingapp.service.FlightService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,25 +23,24 @@ public class FlightController {
     }
 
     @PostMapping
-    public ResponseEntity<FlightEntity> createFlight(@RequestBody FlightEntity flight) {
-        flight.setId(null);
+    public ResponseEntity<FlightResponse> createFlight(@RequestBody FlightRequest flight) {
         return ResponseEntity.ok(flightService.createFlight(flight));
     }
 
     @GetMapping
-    public ResponseEntity<List<FlightEntity>> getAllFlights() {
+    public ResponseEntity<List<FlightResponse>> getAllFlights() {
         return ResponseEntity.ok(flightService.getAllFlights());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FlightEntity> getFlightById(@PathVariable Long id) {
+    public ResponseEntity<FlightResponse> getFlightById(@PathVariable Long id) {
         return ResponseEntity.ok(flightService.getFlightById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<FlightEntity> updateFlight(
+    public ResponseEntity<FlightResponse> updateFlight(
             @PathVariable Long id,
-            @RequestBody FlightEntity updatedFlight
+            @RequestBody FlightRequest updatedFlight
     ) {
         return ResponseEntity.ok(flightService.updateFlight(id, updatedFlight));
     }
@@ -46,5 +49,27 @@ public class FlightController {
     public ResponseEntity<Void> deleteFlight(@PathVariable Long id) {
         flightService.deleteFlight(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/next-24-hours")
+    public ResponseEntity<List<FlightResponse>> getFlightsInNext24Hours() {
+        return ResponseEntity.ok(flightService.getFlightsInNext24Hours());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<FlightResponse>> searchFlights(
+            @RequestParam String toCity,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestParam int numberOfPassenger
+    ) {
+        return ResponseEntity.ok(flightService.searchFlights(toCity, date, numberOfPassenger));
+    }
+
+    @GetMapping("/{id}/available-seats")
+    public ResponseEntity<Boolean> hasAvailableSeats(
+            @PathVariable int id,
+            @RequestParam int requiredSeats
+    ) {
+        return ResponseEntity.ok(flightService.hasAvailableSeats(id, requiredSeats));
     }
 }
